e33c6feae2d31f792316023d6b80e18757ffcb8f
rm86626 <106156564+rm86626@users.noreply.github.com>
2022-11-08 17:56:33 +0800

    react开发环境

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+
+    state={
+        count:0
+    }
+    handdleClick=()=>{
+        let {count}=this.state;
+        this.setState({count:count+1});
+    }
+  render() {
+    let {count}=this.state;
+    console.log("render");
+    return (
+      <div>
+        <h3>计数器</h3>
+        <p>count:{count}</p>
+        <button onClick={this.handdleClick}>+</button>
+      </div>
+    )
+  }
+  componentDidMount(){
+    console.log("同步语句1------>count值:",this.state.count);
+    this.setState((prevState,porps)=>({count:prevState.count+1}));
+    console.log("同步语句1------>count值:",this.state.count);
+    this.setState((prevState,porps)=>({count:prevState.count+1}));
+    console.log("同步语句1------>count值:",this.state.count);
+
+    }
+    
+}
